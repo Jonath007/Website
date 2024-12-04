86fa8cf6750c0c86b8e123d01166afe07f6a333b
Jonath B E <118730722+Jonath007@users.noreply.github.com>
2024-12-04 18:33:22 +0530

    Home.tsx

--- website/src/pages/Home.tsx
+++ website/src/pages/Home.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <img
       src={image} // Use the imported image
       alt="University Campus"
-      className="w-full h-full object-contain"
+      className="w-full h-full object-cover"
       />
         
       </div>
